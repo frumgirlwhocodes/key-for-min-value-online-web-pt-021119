5120d5aa90ce6556faaa5ce3f29cc941bad81c95
lieby weintraub <liebahinda@gmail.com>
2019-03-03 22:13:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@
 def key_for_min_value(name_hash)
   lowest_key= nil 
   lowest_value= nil
-  name_hash.each do |key, value|
+  name_hash.each do |name, value|
   if lowest_value == nil || value < lowest_value  
-    lowest_key= key
+    lowest_key= name
     lowest_value= value 
   end 
   lowest_key
