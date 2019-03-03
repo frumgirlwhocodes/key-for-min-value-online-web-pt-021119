7646ce2d9c0acfc568a6cd34d2f36799602fcfca
lieby weintraub <liebahinda@gmail.com>
2019-03-03 22:11:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   if lowest_value == nil || value < lowest_value  
     lowest_key= key
     lowest_value= value 
-    return lowest_key
+    return key
   end 
 end 
 end
